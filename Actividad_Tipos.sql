01cb74ac957b34b12d1d272774bf535c27c052ce
pablocavestany <pablo.cavestany@gmail.com>
2016-11-29 17:31:37 +0100

    primer comit

---
+++ Actividad_Tipos.sql
@@ -0,0 +1,5 @@
+create table tipos_enteros (
+     primercampo smallint unsigned,
+     segundocampo smallint(5) zerofill unsigned,
+     tercercampo mediumint);
+
---
+++ Tipos_ComaFlotanteYFijo.sql
@@ -0,0 +1,11 @@
+/* create table Tipos_Coma(
+	campo1 float(10,5),
+	campo2 dec(10,5),
+	campo3 boolean,
+	campo4 bit(20));
+
+insert into Tipos_Coma values (
+	23.634819, 5.5, 0, b'1001');
+*/
+
+insert into Tipos_Coma values (8999.9999999, 23.634819, true, 6);
