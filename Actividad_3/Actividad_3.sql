4ceaa55acc669df4125256ab46ed59b7d1abc3c8
pablo cavestany <pablo.cavestany@gmail.com>
2017-03-07 19:04:36 +0100

    Actualizacion

--- Actividad_3/Actividad_3.sql
+++ Actividad_3/Actividad_3.sql
@@ -4,4 +4,10 @@
 -- 2select sum(f.Import) 'Suma total' from factura f where f.Data='20090306';
 -- 3select d.Descripcio from detallfactura d join factura f using (codifactura) where f.DNI='022222222R';
 -- 4select c.Nom, p.Titol from client c join prestec pr on c.DNI=pr.DNI join copia co on co.codipeli=pr.codipeli and co.codicopia=pr.codicopia join pelicula p on p.codipeli=co.codipeli;
-select c.Nom, p.Titol from client c join prestec pr  using(DNI) join pelicula p using(codipeli);
+-- 4select c.Nom, p.Titol from client c join prestec pr using(DNI) join pelicula p using(codipeli);
+-- 5select cl.Nom, count(*) NumPelis from client cl join prestec pr using(DNI) group by cl.Nom;
+-- 6select distinct a.Nom from actor a join pelicula using (codiactor);
+-- 7select Titol from pelicula p,genere g where p.codigenere=g.codigenere and Descripcio='Terror';
+-- 8select p.Titol 'Segunda parte', pe.Titol 'Primera parte' from pelicula p join pelicula pe on p.codipeli=pe.Segonapart;+
+-- 9select p.Titol, count(co.codicopia) 'Num de copias' from pelicula p join copia co using(codipeli) group by p.Titol;
+select f.Import, f.Data from factura f order by 2 desc;
