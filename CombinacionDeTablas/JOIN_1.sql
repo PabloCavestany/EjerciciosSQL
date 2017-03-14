1c083194d9c0f1d57d742cc88952248d2c34920c
pablo cavestany <pablo.cavestany@gmail.com>
2017-03-14 18:07:54 +0100

    Actualización

--- CombinacionDeTablas/JOIN_1.sql
+++ CombinacionDeTablas/JOIN_1.sql
@@ -4,4 +4,4 @@
 -- select city.name, country.name from city,country where city.countrycode=country.code and country.name='Ukraine';
 -- select distinct language from countrylanguage, country where countrylanguage.countrycode=country.code and continent='Africa' order by countrylanguage.language asc;
 -- select sum(city.population) from country, city where city.countrycode=country.code and country.name='Russian Federation';
-select (country.population - sum(city.population)), country.name pais from city, country where city.countrycode=country.code and country.name='Russian Federation';
+-- select (country.population - sum(city.population)), country.name pais from city, country where city.countrycode=country.code and country.name='Russian Federatio
