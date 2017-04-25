b18d447fd1ffacc9f0ed8c44d1b79f20b09234c3
pablo cavestany <pablo.cavestany@gmail.com>
2017-04-25 18:05:47 +0200

    Actualización

--- Procedimientos/PL_MySql.sql
+++ Procedimientos/PL_MySql.sql
@@ -16,4 +16,45 @@ create procedure  segundoC_procedimiento ()
 		select * from city limit 10; 
 		select * from city limit 10; 
 	end//
+-- 3
+
+
+create procedure ejercicio123(parametro int) 
+	select parametro;
+	
+call ejercicio123(3);
+
+
+-- Ejercicio 1-5 ---
+-- 2 (Insertar cliente)
+-- Para cambiar el modo de operación hacemos:
+-- set sql_mode=''; (Así evitamos operar en modo 
+-- STRICT_TRANS_TABLES)
+create procedure ejercicio152(codigoCliente int, nombreCliente varchar(45))
+	insert into cliente(cliente_cod,nombre) 
+		values (codigoCliente,nombreCliente);
+*/
+-- 4
+delimiter //
+create procedure ejercicio154(fecha date)
+begin
+	declare var int;
+	select datediff(fecha,curdate()) into var;
+	select date_format(fecha,'%W %d/%m/%Y');
+	select var;
+end//
+delimiter ;
+
+
+
+
+
+
+
+
+
+
+
+
+
  
